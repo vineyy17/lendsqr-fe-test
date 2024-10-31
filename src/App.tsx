1bcc6f0cb73437171d39189acf353bb20d47f279
Divine Charles <officialkb17@gmail.com>
2024-10-31 13:42:08 +0100

    Structure initial route layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
+import Users from './pages/Users';
+import UserDetails from './pages/UserDetails';
+
 function App() {
-  return <div>Lendsqr</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route index path="users" element={<Users />} />
+          <Route index path="users/:userId" element={<UserDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

f256d356b4757b845370aef88e2b0574cf72daec
Divine Charles <officialkb17@gmail.com>
2024-10-31 22:24:56 +0100

    Create switch organization option component

---
+++ src/components/SwitchOrganizationOption.tsx
@@ -0,0 +1,23 @@
+import '../styles/components/SwitchOrganizationOption.scss';
+import switchDropDown from '../assets/icons/switchDrop.svg';
+import briefcaseIcon from '../assets/icons/briefcase.svg';
+
+const SwitchOrganizationOption = () => {
+  return (
+    <div className="switch">
+      <img
+        src={briefcaseIcon}
+        alt="briefcase icon"
+        className="switch__briefcase"
+      />
+      <p className="switch__text">Switch Organization</p>
+      <img
+        src={switchDropDown}
+        alt="dropdown icon"
+        className="switch__dropdown"
+      />
+    </div>
+  );
+};
+
+export default SwitchOrganizationOption;

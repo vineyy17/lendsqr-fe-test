02c43089b2859137f88d6f3837b3cc2c39fe19c5
Divine Charles <officialkb17@gmail.com>
2024-11-01 21:34:56 +0100

    Build FilterUsers component

--- src/components/FilterUsers.tsx
+++ src/components/FilterUsers.tsx
@@ -1,6 +1,9 @@
 import filter from '../assets/icons/table-filter.svg';
 import '../styles/components/FilterUsers.scss';
 import { Popover } from '@radix-ui/themes';
+import FilterSelect from './FilterSelect';
+import FilterInput from './FilterInput';
+import DateInput from './DateInput';
 
 const FilterUsers = () => {
   return (
@@ -11,7 +14,42 @@ const FilterUsers = () => {
         </Popover.Trigger>
         <Popover.Content>
           <div className="filterUsers__box">
-            <p>Popover</p>
+            <div className="filterUsers__box__group">
+              <label className="filterUsers__box__group__text">
+                Organization
+              </label>
+              <FilterSelect />
+            </div>
+            <div className="filterUsers__box__group">
+              <label className="filterUsers__box__group__text">Username</label>
+              <FilterInput placeholder="User" />
+            </div>
+            <div className="filterUsers__box__group">
+              <label className="filterUsers__box__group__text">Email</label>
+              <FilterInput placeholder="Email" />
+            </div>
+            <div className="filterUsers__box__group">
+              <label className="filterUsers__box__group__text">Date</label>
+              <DateInput placeholder="Date" />
+            </div>
+            <div className="filterUsers__box__group">
+              <label className="filterUsers__box__group__text">
+                Phone Number
+              </label>
+              <FilterInput placeholder="Phone Number" />
+            </div>
+            <div className="filterUsers__box__group">
+              <label className="filterUsers__box__group__text">Status</label>
+              <FilterSelect />
+            </div>
+            <div className="filterUsers__box__flexWrapper">
+              <button className="filterUsers__box__flexWrapper__resetButton">
+                Reset
+              </button>
+              <button className="filterUsers__box__flexWrapper__filterButton">
+                Filter
+              </button>
+            </div>
           </div>
         </Popover.Content>
       </Popover.Root>

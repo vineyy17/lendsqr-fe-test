c08c83e19c6b6a1e1a1494ffc63f1219f08b21d2
Divine Charles <officialkb17@gmail.com>
2024-11-01 09:50:09 +0100

    Add UsersStatsCard component to users page

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,35 @@
+import UsersStatsCard from '../components/UsersStatsCard';
+import '../styles/pages/Users.scss';
+import usersIcon from '../assets/icons/usersCircle.svg';
+import activeUsersIcon from '../assets/icons/activeUsersCircle.svg';
+import usersWithSavingsIcon from '../assets/icons/usersWithSavingsCircle.svg';
+import usersWithLoanIcon from '../assets/icons/usersWithLoansCircle.svg';
+
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div className="users">
+      <p className="users__heading">Users</p>
+      <div className="users__stats">
+        <UsersStatsCard name="Users" icon={usersIcon} stat="2453" />
+        <UsersStatsCard
+          name="Active Users"
+          icon={activeUsersIcon}
+          stat="2453"
+        />
+        <UsersStatsCard
+          name="Users With Savings"
+          icon={usersWithSavingsIcon}
+          stat="12,453"
+        />
+        <UsersStatsCard
+          name="Users With Loans"
+          icon={usersWithLoanIcon}
+          stat="102,453"
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Users;

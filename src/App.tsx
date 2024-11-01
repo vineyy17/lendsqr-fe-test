a30d653c6e3d2cc2c1c76b0db6d1683b564a9184
Divine Charles <officialkb17@gmail.com>
2024-11-01 14:30:46 +0100

    Add radix ui package

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import Users from './pages/Users';
 import UserDetails from './pages/UserDetails';
+import { Theme } from '@radix-ui/themes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="users" />} />
-          <Route path="users" element={<Users />} />
-          <Route path="users/:userId" element={<UserDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Theme>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="users" />} />
+            <Route path="users" element={<Users />} />
+            <Route path="users/:userId" element={<UserDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Theme>
   );
 }
 

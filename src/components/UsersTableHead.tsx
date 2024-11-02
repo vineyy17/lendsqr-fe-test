a59113fe6a3e7cfe598b7bd4fec3e0a0ddb13c59
Divine Charles <officialkb17@gmail.com>
2024-11-02 03:27:21 +0100

    Modify UsersTableHead component

--- src/components/UsersTableHead.tsx
+++ src/components/UsersTableHead.tsx
@@ -4,30 +4,30 @@ import FilterUsers from './FilterUsers';
 const UsersTableHead = () => {
   return (
     <thead className="usersTable__table__head">
-      <td className="usersTable__table__head__group usersTable__table__head__group--org">
+      <th className="usersTable__table__head__group usersTable__table__head__group--org">
         <p className="usersTable__table__head__group__title">Organization</p>
         <FilterUsers />
-      </td>
-      <td className="usersTable__table__head__group usersTable__table__head__group--userName">
+      </th>
+      <th className="usersTable__table__head__group usersTable__table__head__group--userName">
         <p className="usersTable__table__head__group__title">Username</p>
         <FilterUsers />
-      </td>
-      <td className="usersTable__table__head__group usersTable__table__head__group--email">
+      </th>
+      <th className="usersTable__table__head__group usersTable__table__head__group--email">
         <p className="usersTable__table__head__group__title">Email</p>
         <FilterUsers />
-      </td>
-      <td className="usersTable__table__head__group usersTable__table__head__group--phone">
+      </th>
+      <th className="usersTable__table__head__group usersTable__table__head__group--phone">
         <p className="usersTable__table__head__group__title">Phone number</p>
         <FilterUsers />
-      </td>
-      <td className="usersTable__table__head__group usersTable__table__head__group--date">
+      </th>
+      <th className="usersTable__table__head__group usersTable__table__head__group--date">
         <p className="usersTable__table__head__group__title">Date joined</p>
         <FilterUsers />
-      </td>
-      <td className="usersTable__table__head__group usersTable__table__head__group--status">
+      </th>
+      <th className="usersTable__table__head__group usersTable__table__head__group--status">
         <p className="usersTable__table__head__group__title">Status</p>
         <FilterUsers />
-      </td>
+      </th>
     </thead>
   );
 };

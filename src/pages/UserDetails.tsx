38b04bc5a425558ed9c29ba3366854f72d7fce3c
Divine Charles <officialkb17@gmail.com>
2024-11-03 20:41:53 +0100

    Retrieve user details from localstorage into user details page

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -6,9 +6,13 @@ import PersonalInformation from '../components/PersonalInformation';
 import EducationInformation from '../components/EducationInformation';
 import SocialsInformation from '../components/SocialsInformation';
 import GuarantorInformation from '../components/GuarantorInformation';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { UserDetails as UserDetailsType } from '../types/userDetails';
 
 const UserDetails = () => {
   const goBack = useGoBack();
+
+  const [user, _] = useLocalStorage<UserDetailsType>('user');
 
   return (
     <div className="userDetails">
@@ -22,13 +26,13 @@ const UserDetails = () => {
         <p className="userDetails__top__text">Back to Users</p>
       </div>
       <div className="userDetails__overview">
-        <UserDetailsOverview />
+        <UserDetailsOverview info={user.accountInfo} />
       </div>
       <div className="userDetails__info">
-        <PersonalInformation />
-        <EducationInformation />
-        <SocialsInformation />
-        <GuarantorInformation />
+        <PersonalInformation info={user.personalInfo} />
+        <EducationInformation info={user.educationEmployment} />
+        <SocialsInformation info={user.socials} />
+        <GuarantorInformation info={user.guarantor} />
       </div>
     </div>
   );

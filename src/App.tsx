8b0163dfe40843209ef7066b0f2ceb7b868c05b4
Divine Charles <officialkb17@gmail.com>
2024-11-03 07:01:24 +0100

    Add React Query Provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,34 @@ import AppLayout from './pages/AppLayout';
 import Users from './pages/Users';
 import UserDetails from './pages/UserDetails';
 import { Theme } from '@radix-ui/themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
     <Theme>
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="users" />} />
-            <Route path="users" element={<Users />} />
-            <Route path="users/:userId" element={<UserDetails />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="users" />} />
+              <Route path="users" element={<Users />} />
+              <Route path="users/:userId" element={<UserDetails />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </Theme>
   );
 }

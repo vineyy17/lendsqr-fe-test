2c3aa0313e7f894fdd1e2d389d538c667f16cfdd
Divine Charles <officialkb17@gmail.com>
2024-11-04 11:04:06 +0100

    Reflect filter state in users table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -7,17 +7,55 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import { User } from '../types/userTypes';
 import '../styles/components/Pagination.scss';
 import Pagination from './Pagination';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFilterStore } from '../store/filterStore';
+import notFound from '../assets/images/notFound.webp';
+import { useSearchParams } from 'react-router-dom';
 
 const UsersTable = () => {
   const { isLoading, users, error } = useUsers();
+  const { filteredUsers, resetFilters } = useFilterStore();
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const paginatedUsers = users?.slice(
+  const displayedUsers = filteredUsers || users;
+
+  const paginatedUsers = displayedUsers?.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage,
   );
+
+  useEffect(() => {
+    setCurrentPage(1);
+    setSearchParams({ ...Object.fromEntries(searchParams), page: '1' });
+  }, [filteredUsers]);
+
+  if (filteredUsers?.length === 0) {
+    return (
+      <div className="usersTable">
+        <table className="usersTable__table">
+          <UsersTableHead />
+          <div className="usersTable__noResults">
+            <img
+              src={notFound}
+              alt="404 image"
+              className="usersTable__noResults__image"
+            />
+            <p className="usersTable__noResults__text">
+              No user matches the filter criteria
+            </p>
+            <button
+              className="usersTable__noResults__button"
+              onClick={resetFilters}
+            >
+              Display all users
+            </button>
+          </div>
+        </table>
+      </div>
+    );
+  }
 
   if (isLoading)
     return (
@@ -43,7 +81,7 @@ const UsersTable = () => {
           {paginatedUsers?.map((user: User, index: number, array) => (
             <UsersTableRow
               user={user}
-              key={index}
+              key={user.id}
               isLast={index === array.length - 1}
             />
           ))}
@@ -51,7 +89,7 @@ const UsersTable = () => {
       </div>
 
       <Pagination
-        totalItems={users?.length ?? 0}
+        totalItems={displayedUsers?.length ?? 0}
         itemsPerPage={itemsPerPage}
         onPageChange={setCurrentPage}
         onItemsPerPageChange={setItemsPerPage}

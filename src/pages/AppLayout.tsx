3ac502b35dcaacfc479259ad8bba2c9c66f139aa
Divine Charles <officialkb17@gmail.com>
2024-11-05 08:41:29 +0100

    Make AppLayout responsive

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -2,14 +2,24 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import '../styles/pages/AppLayout.scss';
+import { useState } from 'react';
 
 const AppLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="appLayout">
-      <Header />
-      <Sidebar />
+    <div className={`appLayout ${isSidebarOpen ? 'appLayout--sideOpen' : ''}`}>
+      <Header toggleSidebar={toggleSidebar} />
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        closeSidebar={() => setIsSidebarOpen(false)}
+      />
       <main className="appLayout__main">
-        <div className="appLayout__container">
+        <div className="appLayout__main__container">
           <Outlet />
         </div>
       </main>

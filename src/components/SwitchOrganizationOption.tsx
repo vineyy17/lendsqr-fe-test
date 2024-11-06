fed43845b6d226b502779e82dca779153c40e638
Divine Charles <officialkb17@gmail.com>
2024-11-06 12:50:14 +0100

    Add pop-up to switch organization option in sidebar

--- src/components/SwitchOrganizationOption.tsx
+++ src/components/SwitchOrganizationOption.tsx
@@ -1,8 +1,16 @@
 import '../styles/components/SwitchOrganizationOption.scss';
 import switchDropDown from '../assets/icons/switchDrop.svg';
 import briefcaseIcon from '../assets/icons/briefcase.svg';
+import { useState } from 'react';
 
 const SwitchOrganizationOption = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Toggle the popup visibility when the switch div is clicked
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="switchWrapper">
       <div className="switch">
@@ -16,8 +24,21 @@ const SwitchOrganizationOption = () => {
           src={switchDropDown}
           alt="dropdown icon"
           className="switch__dropdown"
+          onClick={togglePopup}
         />
       </div>
+
+      {isOpen && (
+        <div className="switch__popover">
+          {['Organization 1', 'Organization 2', 'Organization 3'].map(
+            (org, index) => (
+              <p key={index} onClick={togglePopup}>
+                {org}
+              </p>
+            ),
+          )}
+        </div>
+      )}
     </div>
   );
 };

20c9122637ce2412a393f2e77e868c02157a2b76
Divine Charles <officialkb17@gmail.com>
2024-11-08 06:26:15 +0100

    Fix breaking mock api url

--- src/services/fetchUsers.ts
+++ src/services/fetchUsers.ts
@@ -2,7 +2,7 @@ import { User } from '../types/userTypes';
 
 export const fetchUsers = async (): Promise<User[]> => {
   const response = await fetch(
-    'https://run.mocky.io/v3/aaf962b3-813e-401b-a2c5-dd6a3c17b011',
+    'https://run.mocky.io/v3/369cb0d0-82a6-4d9e-ba15-59524eaeb4e5',
   );
   if (!response.ok) {
     throw new Error('Failed to fetch users');
--- src/services/fetchUsersOverview.ts
+++ src/services/fetchUsersOverview.ts
@@ -2,7 +2,7 @@ import { UsersOverview } from '../types/userTypes';
 
 export const fetchUsersOverview = async (): Promise<UsersOverview> => {
   const response = await fetch(
-    'https://run.mocky.io/v3/0c763a22-d589-4ffe-818e-ddbc88a197a9',
+    'https://run.mocky.io/v3/da477313-5000-4528-8240-e9be6b110c41',
   );
   if (!response.ok) {
     throw new Error('Failed to fetch users overview');
